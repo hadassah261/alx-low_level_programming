867fa25497ef10a6ca1d5cba71d119ac66640146
hadassah261 <babatopeabisola@gmail.com>
2023-06-15 14:54:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE -o ${CFILE%.c}.s
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
+}
