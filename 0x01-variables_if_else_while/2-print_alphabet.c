baf31d3ee26db9479d160a7d693f5ff32b2aa3d4
hadassah261 <babatopeabisola@gmail.com>
2023-06-16 04:06:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A code that assigns a random number to the variable n each time it is executed and prints the last digit of the number stored in the variable n
+ * Return: 0
+ */
+int main(void) {
+    int n;
+    srand(time(NULL));
+    n = rand();
+    printf("Last digit of %d is ", n);
+    if (n % 10 > 5) {
+        printf("%d and is greater than 5\n", n % 10);
+    } else if (n % 10 == 0) {
+        printf("%d and is 0\n", n % 10);
+    } else {
+        printf("%d and is less than 6 and not 0\n", n % 10);
+    }
+    return 0;
+}
